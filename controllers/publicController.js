f7022763e1ba4a19ffc7daf582b20a15fd5705e0
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2025-06-28 13:51:31 +0200

    Do public download function

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler")
 const folderQuery = require("../db/folderQuery")
 const fileQuery = require("../db/fileQuery")
+const { supabase } = require("../supabase/supabase");
 
 exports.publicFolderGet = asyncHandler(async (req, res) => {
     const {sharedLinkId} = req.params;
@@ -14,9 +15,9 @@ exports.publicFolderGet = asyncHandler(async (req, res) => {
         throw error;
     }
 
-    if (new Date() > sharedLink.expiresAt) {
+    if (new Date() > new Date(sharedLink.expiresAt)) {
         const error = new Error("This link has already expired")
-        error.status = 404;
+        error.status = 410;
 
         throw error;
     }
@@ -79,4 +80,51 @@ exports.publicFolderPost = asyncHandler(async (req, res) => {
     req.session.sharedLink = `http://localhost:3000/public/${link.id}`;
 
     res.redirect("/uploads/" + (folderId ? folderId : ""));
+})
+
+exports.downloadFileGet = asyncHandler(async (req, res) => {
+    const { sharedLinkId, fileId } = req.params
+
+    const sharedLink = await folderQuery.getSharedLink(sharedLinkId)
+    
+    if (!sharedLink) {
+        const error = new Error("This link does not exist")
+        error.status = 404;
+
+        throw error;
+    }
+
+    if (new Date() > new Date(sharedLink.expiresAt)) {
+        const error = new Error("This link has already expired")
+        error.status = 410;
+
+        throw error;
+    }
+
+    const file = await fileQuery.getFile(sharedLink.userId, fileId)
+
+    if (!file) {
+        const error = new Error("File not found")
+        error.status = 404;
+        throw error;
+    }
+
+    const { data, error } = await supabase.storage
+        .from("uploads")
+        .download(file.bucketId);
+
+    if (error) {
+        const err = new Error("Error downloading file");
+        err.status = 500;
+        throw err;
+    }
+
+    const arrayBuffer = await data.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    res.setHeader("Content-Disposition", `attachment; filename="${file.name}"`);
+    res.setHeader("Content-Type", data.type || "application/octet-stream");
+    res.setHeader("Content-Length", buffer.length);
+
+    res.send(buffer);
 })
--- routes/publicRouter.js
+++ routes/publicRouter.js
@@ -4,5 +4,6 @@ const publicRouter = Router()
 
 publicRouter.get(["/:sharedLinkId", "/:sharedLinkId/:folderId"], publicController.publicFolderGet)
 publicRouter.post(["/:folderId"], publicController.publicFolderPost)
+publicRouter.get("/:sharedLinkId/:fileId/download", publicController.downloadFileGet)
 
 module.exports = publicRouter
